676e87820bdb22732edd07c03708611be8a60dc7
unknown <you@example.com>
2022-08-04 17:21:45 +0700

    chu vi hình tròn

---
+++ chu_vi_hinh_tron.js
@@ -0,0 +1,9 @@
+let inputBankinh;
+
+inputBankinh = prompt('Bán kính của hình tròn');
+
+let Bankinh = parseFloat(inputBankinh)
+
+let x = 3.14*2;
+let area = (Bankinh*x);
+document.write('Bán kính hình tròn' + area);
