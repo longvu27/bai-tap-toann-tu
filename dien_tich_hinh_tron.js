ad395fe2a92d1ec19705708e758c00df8b211568
unknown <you@example.com>
2022-08-04 17:17:33 +0700

    diện tích hình tròn

---
+++ dien_tich_hinh_tron.js
@@ -0,0 +1,9 @@
+let inputBankinh;
+
+inputBankinh = prompt('Bán kính của hình tròn');
+
+let Bankinh = parseFloat(inputBankinh)
+
+let x = 3.14;
+let area = (Bankinh*Bankinh*x);
+document.write('Diện tích hình tròn' + area);
